ed1de6fbe9b3df910f04d666a23b23d0bdb5a546
Sammie Chen <chs1208@gmail.com>
2019-08-24 15:15:31 +0800

    filter

--- review.py
+++ review.py
@@ -8,7 +8,7 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data)) #印出讀取進度
 print('資料計有：', len(data), '筆') #印出資料筆數
-
+print(data[0]) #印出第＿筆資料內容
 
 sum_len = 0
 
@@ -17,5 +17,16 @@ for d in data:
 print('留言計有： ', sum_len, '字')
 print('留言平均為：', sum_len / len(data), '字')
 
+new = []
+for d in data:
+	if len(d) > 100:
+		new.append(d)
+print('共有', len(new), '筆留言大於100')
 
-print(data[0]) #印出第＿筆資料內容
+better = []
+for d in data:
+	if 'better' in d:
+		better.append(d)
+print('共有', len(better), '筆留言提到better')
+print(better[2])
+
