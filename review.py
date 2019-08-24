8a5944bac062e6f9a5185a1696dc4d841e927cd6
Sammie Chen <chs1208@gmail.com>
2019-08-24 14:47:39 +0800

    average data

--- review.py
+++ review.py
@@ -7,6 +7,15 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data)) #印出讀取進度
+print('資料計有：', len(data), '筆') #印出資料筆數
 
-print(len(data)) #印出資料筆數
+
+sum_len = 0
+
+for d in data:
+	sum_len += len(d)
+print('留言計有： ', sum_len, '字')
+print('留言平均為：', sum_len / len(data), '字')
+
+
 print(data[0]) #印出第＿筆資料內容
