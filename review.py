ac29314b036261031406cf020d947eebe624fa53
Sammie Chen <chs1208@gmail.com>
2019-08-24 14:38:08 +0800

    count data

--- review.py
+++ review.py
@@ -1,7 +1,12 @@
 data = []
+count = 0
 
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
-print(len(data))
-print(data[0])
+		count += 1
+		if count % 1000 == 0:
+			print(len(data)) #印出讀取進度
+
+print(len(data)) #印出資料筆數
+print(data[0]) #印出第＿筆資料內容
